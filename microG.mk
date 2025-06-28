9d5ec367d21d6a219a812f2d52b45d0564bd57a7
hrzndelta <widiadinata1130@proton.me>
2025-06-28 12:08:28 +0800

    adding packages to microG.mk

--- microG.mk
+++ microG.mk
@@ -3,8 +3,19 @@ PRODUCT_PACKAGE += \
 	AuroraStore \
 	Droid-ify \
 	FakeStore \
-	GMSCore \
-	GSFProxy
+	GmsCore \
+	GsfProxy
 
 # Include package from GMS vendor
 PRODUCT_PACKAGE += \
+	DevicePersonalizationPrebuiltPixel \
+	DocumentsUIGoogle \
+	Flipendo \
+	SettingsIntelligenceGooglePrebuilt
+
+# Pixel goodies
+ifneq ($(filter sailfish marlin walleye taimen blueline crosshatch sargo bonito flame coral sunfish bramble redfin barbet oriole raven bluejay panther cheetah lynx tangorpro felix shiba husky akita tokay caiman komodo comet frankel blazer mustang, $(LINEAGE_BUILD)),)
+PRODUCT_PACKAGES += \
+	DeviceConnectivityServicePrebuilt_25.01.00 \
+	SCONE
+endif
